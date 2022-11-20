aeb1624d8c29c6c3f09aabb360daac371b642370
gilous007 <gil.utard@gmail.com>
2022-11-20 21:03:53 +0100

    add init

---
+++ init.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "syscall.h"
+
+
+int main() {
+        int i=0;
+	i=spawn("./bar.so");
+	printf("./bar.so lance : %d\n",i);
+	i=spawn("./foo.so");
+	printf("./foo.so lance : %d\n",i);
+	while (1) {
+	      printf("init %d\n",mlk_clock());
+	      //for (i=0;i<100000000;i++);
+	      mlk_sleep(10);
+	  }
+
+}
